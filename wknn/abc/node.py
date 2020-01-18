58086886e8eb30e168df98f60ccb2a373374c9d1
witold kepczynskii <witold.kepczynski@gmail.com>
2020-01-18 18:23:43 +0100

    first commit

---
+++ wknn/abc/node.py
@@ -0,0 +1,29 @@
+import abc
+
+class Node(object):
+    ___metaclass__ = abc.ABCMeta
+
+    # X vector
+    def X_getter(self)->list:
+        return []
+    def X_setter(self,new_value)->None:
+        return
+    X = abc.abstractproperty(fget=X_getter,fset=X_setter,doc="Input vector for Node")
+    #
+    # W vector
+    def W_getter(self)->list:
+        return []
+    def W_setter(self,new_value)->None:
+        return
+    W = abc.abstractproperty(fget=W_getter,fset=W_setter,doc="Input weight vector for Node")
+    #
+    # Y 
+    def y_getter(self)->float:
+        return []
+    W = abc.abstractproperty(fget=y_getter,fset=None,doc="Output value of Node")
+    #
+    # Y 
+    def ytrain_getter(self)->float:
+        return []
+    W = abc.abstractproperty(fget=ytrain_getter,fset=None,doc="Intended output value of Node")
+    #
